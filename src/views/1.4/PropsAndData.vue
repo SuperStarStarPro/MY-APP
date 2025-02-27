f33958eb65d6a8cd7796b173c72c24f876fb1419
PotatoWarmPro <3184724124@qq.com>
2025-02-27 17:56:35 +0800

    修改

--- src/views/1.4/PropsAndData.vue
+++ src/views/1.4/PropsAndData.vue
@@ -3,7 +3,8 @@
         <p>props.info:{{ info }}</p>
         <p>props.name:{{ name }}</p>
         <p>props.list:{{ list }}</p>
-        <p>props.a:{{ a }}</p>
+        <p>data.a:{{ a }}</p> <!-- 注意这里应该是 data.a -->
+        <p>data.b:{{ b }}</p> <!-- 添加对 b 的显示 -->
         <p>
             <button @click="handleBChange">change data.b</button>
         </p>
@@ -13,22 +14,24 @@
 export default {
   name: 'PropsAndData',
   props: {
-    info:Object,
-    name:String,
-    list:Array
+    info: Object,
+    name: String,
+    list: Array
   },
   data() {
     return {
-        a:"hello",
+        a: "hello",
+        b: "" // 添加 b 的初始值
     };
-},
-updated(){
+  },
+  updated() {
     console.log("触发 PropsAndData 更新");
-},
-methods: {
+  },
+  methods: {
     handleBChange() {
         this.b = "vue" + Date.now();
-        console.log("data.b 发生了变化，但并没有触发组件更新",this.b);
+        console.log("data.b 发生了变化，会触发组件更新", this.b);
     }
+  }
 }
 </script>
